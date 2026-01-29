c73267fd1e45f893d1f6e9cfc6835533f2e16387
Ninad Sachania <ninad.sachania@gmail.com>
2026-01-29 23:46:48 +0530

    Add ability to build on Linux

---
+++ build.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+mkdir -p build
+
+pushd build
+
+gcc -g -Wall -Wextra -std=c++14 ../source/main.cpp -o main \
+    -I../raylib_linux_amd64/include \
+    -L../raylib_linux_amd64/lib \
+    -l:libraylib.a -lm
+
+cp ../source_art/player.png .
+cp ../source_art/tilemap.png .
+
+popd
+
+echo ""
+echo "Build complete!"
+
