7a3d9066791b954d2b927544505c72b45930415b
Ninad Sachania <ninad.sachania@gmail.com>
2026-02-02 22:29:08 +0530

    Stop build script on first error

--- build.bat
+++ build.bat
@@ -1,18 +1,19 @@
 @echo off
+setlocal
 
-if not exist build mkdir build
+if not exist build mkdir build || exit /b 1
 
-pushd build
+pushd build || exit /b 1
 
 cl /std:c++14 /Zi /W4 /Fe:main.exe ..\source\main.cpp ^
 	/I..\raylib_win64_msvc16\include ^
 	/link ^
 	/LIBPATH:..\raylib_win64_msvc16\lib ^
 	raylib.lib user32.lib gdi32.lib winmm.lib shell32.lib msvcrt.lib ^
-	/NODEFAULTLIB:libcmt /NODEFAULTLIB:msvcrtd
+	/NODEFAULTLIB:libcmt /NODEFAULTLIB:msvcrtd || exit /b 1
 
-copy ..\source_art\player.png .
-copy ..\source_art\tilemap.png .
+copy ..\source_art\player.png  . || exit /b 1
+copy ..\source_art\tilemap.png . || exit /b 1
 
 popd
 
